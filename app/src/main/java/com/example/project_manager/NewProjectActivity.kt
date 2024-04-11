37311b08737da2a397668d67eccb0de6a60bf46f
francesca basile <20030475@studenti.uniupo.it>
2024-04-11 10:50:38 +0200

    ho iniziato a creare le variabili che servono per salvare il task appena creato insieme all'array per salvare anche tutti i sottotask.

--- app/src/main/java/com/example/project_manager/NewProjectActivity.kt
+++ app/src/main/java/com/example/project_manager/NewProjectActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class NewProjectActivity : AppCompatActivity() {
@@ -18,6 +19,7 @@ class NewProjectActivity : AppCompatActivity() {
 
         val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
         val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        val buttonSave=findViewById<Button>(R.id.buttonSave)
 
         buttonAdd.setOnClickListener {
             val editText = EditText(this)
@@ -26,7 +28,22 @@ class NewProjectActivity : AppCompatActivity() {
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
             editText.hint = "Nome del sotto-task"
-            linearLayout.addView(editText)
+            linearLayout.addView(editText,linearLayout.childCount-1)
+        }
+
+        buttonSave.setOnClickListener {
+            val title=findViewById<EditText>(R.id.titleNewProject)
+            val leader=findViewById<Spinner>(R.id.projectLeaderSpinner)
+            val scadenza=findViewById<Button>(R.id.pickDate)
+            val subTask=ArrayList<String>()
+            for(i in 0 until linearLayout.childCount -1){
+                val editText=linearLayout.getChildAt(i) as EditText
+                val subTaskName=editText.text.toString()
+                if(subTaskName.isNotEmpty()){
+                    subTask.add(subTaskName)
+                }
+            }
+
         }
 
     }
